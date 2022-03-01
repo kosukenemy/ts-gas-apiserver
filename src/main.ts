0ea8925d4b982fd3eccba3d9849c3e4dd7669b30
kosukenemy <kosukenemy@gmail.com>
2022-03-02 08:19:09 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,53 @@
-const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("シート1");
-const authToken = PropertiesService.getScriptProperties().getProperty('authToken') || ''
 
+
+interface AppsScriptHttpRequestEvent {
+  parameter: { [key: string]: string };
+  contextPath: string;
+  contentLength: number;
+  queryString: string;
+  parameters: { [key: string]: string[] };
+}
+
+interface AppsScriptHttpRequestEventPostData {
+  length: number;
+  type: string;
+  contents: string;
+  name: string;
+}
+
+interface DoPost extends AppsScriptHttpRequestEvent {
+  postData: AppsScriptHttpRequestEventPostData;
+}
+
+interface DoGet extends AppsScriptHttpRequestEvent {
+  pathInfo: string;
+}
+
+interface AppUsersType {
+  user_id: string;
+  user_name: string;
+  user_email: string;
+  user_password: string;
+}
+
+interface ObjectType {
+  id: string;
+  title: string;
+  content: string;
+}
+
+const sheetName = "シート1";
+const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
+
+const bookItems = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('books');
+const loginUsers = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('appUsers');
+const authToken = PropertiesService.getScriptProperties().getProperty('authToken') || ''
+const logger = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("log");
 
 function test () {
   PropertiesService.getScriptProperties().setProperties({
     authToken: authToken
   })
-  console.log(authToken);
 }
 
 function response(message: object){
@@ -15,16 +56,27 @@ function response(message: object){
   res.setContent(JSON.stringify(message));
 }
 
-function doGet(){
+function doGet(e: DoGet){
+  const path = e.pathInfo; 
+
+  const logger = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("log");
+  logger!.getRange('A4').setValue(e);
+  
+
+  if ( path === "users" ) {
+      return ContentService.createTextOutput(JSON.stringify(getUsers(), null, 2))
+      .setMimeType(ContentService.MimeType.JSON);
+  }
+
+  if ( path === "items" ) {
+      return ContentService.createTextOutput(JSON.stringify(getItem(), null, 2))
+      .setMimeType(ContentService.MimeType.JSON);
+  }
+
   return ContentService.createTextOutput(JSON.stringify(getItem(), null, 2))
   .setMimeType(ContentService.MimeType.JSON);
 }
 
-interface ObjectType {
-  id: string;
-  title: string;
-  content: string;
-}
 
 function getItem(){
   const rows = sheet!.getDataRange().getValues();
@@ -39,16 +91,29 @@ function getItem(){
   })
 }
 
-function doPost(event: any) {
+function getUsers(){
+  const rows = loginUsers!.getDataRange().getValues();
+  const keys = rows.splice(0,1)[0];
+  return rows.map((row) => {
+    const obj = {} as AppUsersType;
+    row.forEach((item, index) => {
+      const key: keyof AppUsersType = keys[index];
+      obj[key] = item;
+    });
+    return obj;
+  })
+}
+
+
+
+function doPost(event: DoPost) {
   let contents;
+
+  
+  logger!.getRange('A1').setValue(event);
+
   try {
     contents = JSON.parse(event.postData.contents);
-    const logger = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("log");
-    logger!.getRange('A1').setValue(event);
-
-    const title = event.parameters["title"];
-    logger!.getRange('A3').setValue(title)
-
   } catch(event){
     return response({ error: 'JSONの形式が正しくありません' })
   }
@@ -57,11 +122,16 @@ function doPost(event: any) {
     return response({ error: '認証に失敗しました' })
   }
 
-  const { method = '', params = {} } = contents;
+  const { method = '', params = {}, state = '' } = contents;
+
+  logger!.getRange('A5').setValue(state);
 
   let result;
   if ( method === "POST" ) {
     result = onPost(params);
+  }
+  if ( method === "POST" && state === "login" ) {
+    result = onLogin(params)
   }
   if ( method === "DELETE" ) {
     result = onDelete(params);
@@ -72,6 +142,14 @@ function doPost(event: any) {
   return result;
 }
 
+
+function onLogin(params: AppUsersType) {
+  const { user_id, user_name, user_email, user_password } = params;
+  logger!.getRange('A5').setValue("ログインされました");
+  logger!.getRange('B5').setValue(user_name);
+
+  return ContentService.createTextOutput(user_id);
+}
 
 function onPost(params: ObjectType){
   const { id, title, content } = params;
